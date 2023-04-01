7933a5f0bd1bff87128f0d98fbae16a0b982b669
Nton23 <huutonau@gmail.com>
2023-03-31 23:23:08 -0700

    Add 3 global variables

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function divide (firstNumber, secondNumber) {
     let result = firstNumber / secondNumber;
     return result;
 }
+//Create 3 global variables: first input, operator sign, second input
+let firstNumberInput = 0;
+let secondNumberInput = 0;
+let operatorInput = "";
+
+//Create an operate function takes an operator and 2 numbers
+//then calls one of the above function on the numbers
+function operate(firstNumber, secondNumber, operatorSign) {
+    
+};
