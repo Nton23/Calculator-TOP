898a99938f14ade32223864063e9f6ff7b59e541
Nton23 <huutonau@gmail.com>
2023-03-31 23:17:50 -0700

    Add operators function

---
+++ script.js
@@ -0,0 +1,17 @@
+//create operators function 
+function add (firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+function substract (firstNumber, secondNumber) {
+    let result = firstNumber - secondNumber;
+    return result;
+}
+function multiply (firstNumber, secondNumber) {
+    let result = firstNumber * secondNumber;
+    return result;
+}
+function divide (firstNumber, secondNumber) {
+    let result = firstNumber / secondNumber;
+    return result;
+}
